79baa0115c20845d5f2263803a2c9ce4ad92bf29
junting <junting0218@gmail.com>
2025-04-01 20:14:55 +0800

    feat: printing log, but accidentally changed the logic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def add(a, b):
     # Return the sum of a and b
-    return a + b
+    print("Adding numbers...But accidentally changed the logic")
+    return abs(a + b)
 
 if __name__ == '__main__':
     result = add(2, 3)
